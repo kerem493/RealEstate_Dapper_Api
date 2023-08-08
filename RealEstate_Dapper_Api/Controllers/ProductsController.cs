92ea949e9537b01698176b73c910ab98076d2be0
kerem493 <kerem.493@hotmail.com>
2023-08-08 15:40:55 +0300

    ilanlardaki ürünleri gösteren api bağlantıları yapıldı ve sql sorguları ile kategori numaraları yerine kategorinin isimleri görünür hale getirildi...

---
+++ RealEstate_Dapper_Api/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate_Dapper_Api.Repositories.ProductRepository;
+
+namespace RealEstate_Dapper_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductrepository _productrepository;
+
+        public ProductsController(IProductrepository productrepository)
+        {
+            _productrepository = productrepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductList()
+        {
+            var values = await _productrepository.GetAllProductAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("ProductLListWithCategory")]
+        public async Task<IActionResult> ProductLListWithCategory()
+        {
+            var values = await _productrepository.GetAllProductWithCategoryAsync();
+            return Ok(values);
+        }
+    }
+}
